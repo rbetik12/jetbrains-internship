a1338b680b2eab086d68c99b389f10e0b27c1775
rbetik12 <mr.prikota@gmail.com>
2021-04-05 17:06:59 +0300

    Added file dialog to select dictionary to search in

--- DictionaryUI/JetBrains.cpp
+++ DictionaryUI/JetBrains.cpp
@@ -1,16 +1,17 @@
 ﻿#include <memory>
 #include <vector>
 #include <string>
+#include <iostream>
+#include <unordered_set>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 #include <imgui.h>
+#include "UI/imfilebrowser.h"
 #include "Utils/Init.h"
 #include "UI/UI.h"
 #include "Utils/Search/Search.h"
 #include "Utils/Threading/TaskScheduler.h"
-#include <iostream>
-#include <unordered_set>
 
 #define INPUT_BUFFER_SIZE 128
 
@@ -41,10 +42,6 @@ bool IsInputChanged(char* currentBuf, char* prevBuff) {
 bool isEnterPressed = false;
 
 int main() {
-    FILE* dictionary = fopen("big-dic.txt", "r");
-    if (!dictionary) {
-        return -1;
-    }
     GLFWwindow* window = InitGLFW();
 
     if (!window) {
@@ -68,6 +65,8 @@ int main() {
     TaskScheduler scheduler;
     int pageNum = 0;
     const int pageSize = 32;
+    FILE* dictionary = nullptr;
+    ImGui::FileBrowser fileDialog;
 
     // We reserve here so much elements to prevent future allocation in a vector. In such way I fixed UI flickering on first search run.
     // In either way program will consume abount 50 mb of ram, so we're just doing that earlier.
@@ -82,51 +81,71 @@ int main() {
             SetFullscreenUI(window);
             ImGui::Begin("Searcher", nullptr, ImGuiWindowFlags_NoResize|ImGuiWindowFlags_NoMove);
             ImGui::InputText("Write here word you want to find", inputBuffer, INPUT_BUFFER_SIZE);
-            if (ImGui::Button("Search") || isEnterPressed) {
+
+            if (!dictionary) {
+                ImGui::TextColored(ImVec4(1, 0, 0, 1), "No active dictionary!");
+            }
+
+            if (dictionary && (ImGui::Button("Search") || isEnterPressed)) {
                 isEnterPressed = false;
                 Search(searchResults, scheduler, dictionary, inputBuffer);
                 pageNum = 0;
             }
 
+            ImGui::SameLine();
+            if (ImGui::Button("Open dictionary")) {
+                fileDialog.Open();
+            }
+
+            fileDialog.Display();
+
+            if (fileDialog.HasSelected()) {
+                dictionary = fopen(fileDialog.GetSelected().string().c_str(), "r");
+                searchResults.clear();
+                fileDialog.ClearSelected();
+            }
+
             if (!scheduler.IsSearchFinished()) {
                 ImGui::TextColored(ImVec4(1, 0, 1, 1), "Search in progress");
             }
+            
+            if (dictionary) {
+                // Here we draw "Next" and "Prev" buttons for pagination UI part. We draw them only if there are more search results than page size
+                if (searchResults.size() > pageSize) {
+                    if (ImGui::Button("Prev")) {
+                        if (pageNum * pageSize - pageSize >= 0) {
+                            pageNum -= 1;
+                        }
+                    }
+                    ImGui::SameLine();
+                    if (ImGui::Button("Next")) {
+                        if (pageNum * pageSize + pageSize < searchResults.size()) {
+                            pageNum += 1;
+                        }
+                    }
+                    ImGui::Text("Page %d/%d", pageNum, searchResults.size() / pageSize);
+                }
 
-            // Here we draw "Next" and "Prev" buttons for pagination UI part. We draw them only if there are more search results than page size
-            if (searchResults.size() > pageSize) {
-                if (ImGui::Button("Prev")) {
-                    if (pageNum * pageSize - pageSize >= 0) {
-                        pageNum -= 1;
+                // Here we print search results to the screen with pagination
+                ImGui::TextColored(ImVec4(1, 1, 0, 1), "Search results");
+                ImGui::BeginChild("Scrolling");
+                if (searchResults.size() <= pageSize) {
+                    for (size_t i = 0; i < searchResults.size(); i++) {
+                        ImGui::TextUnformatted(searchResults[i].c_str());
                     }
                 }
-                ImGui::SameLine();
-                if (ImGui::Button("Next")) {
-                    if (pageNum * pageSize + pageSize < searchResults.size()) {
-                        pageNum += 1;
+                else {
+                    size_t upperBound = pageSize + pageNum * pageSize;
+                    // To prevent out of bounds for vector
+                    if (upperBound >= searchResults.size()) {
+                        upperBound = searchResults.size() - 1;
                     }
-                }                
-                ImGui::Text("Page %d/%d", pageNum, searchResults.size() / pageSize);
-            }
-
-            // Here we print search results to the screen with pagination
-            ImGui::TextColored(ImVec4(1, 1, 0, 1), "Search results");
-            ImGui::BeginChild("Scrolling");
-            if (searchResults.size() <= pageSize) {
-                for (size_t i = 0; i < searchResults.size(); i++) {
-                    ImGui::TextUnformatted(searchResults[i].c_str());
+                    for (size_t i = pageNum * pageSize; i < upperBound; i++) {
+                        ImGui::TextUnformatted(searchResults[i].c_str());
+                    }
                 }
+                ImGui::EndChild();
             }
-            else {
-                size_t upperBound = pageSize + pageNum * pageSize;
-                // To prevent out of bounds for vector
-                if (upperBound >= searchResults.size()) {
-                    upperBound = searchResults.size() - 1;
-                }
-                for (size_t i = pageNum * pageSize; i < upperBound; i++) {
-                    ImGui::TextUnformatted(searchResults[i].c_str());
-                }
-            }
-            ImGui::EndChild();
             ImGui::End();
         }
         UIEnd(window);
