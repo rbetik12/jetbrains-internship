95b11626d78ebdc58aed1a7f909c26531a9834c3
rbetik12 <mr.prikota@gmail.com>
2021-04-09 01:09:02 +0300

    Added triangle drawing

--- TriangleApp/TriangleApp.cpp
+++ TriangleApp/TriangleApp.cpp
@@ -3,6 +3,14 @@
 #include <iostream>
 #include <functional>
 
+template <class T> void SafeRelease(T** ppT) {
+    if (*ppT) {
+        (*ppT)->Release();
+        *ppT = NULL;
+    }
+}
+
+
 TriangleApp* TriangleApp::instance = nullptr;
 
 void TriangleApp::Create(HINSTANCE hInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -11,11 +19,11 @@ void TriangleApp::Create(HINSTANCE hInstance, LPSTR lpCmdLine, int nCmdShow) {
         isPresent = true;
 
         //Opens console for debug purpose. Mem leak!
-        FILE* fp;
+        /*FILE* fp;
         AllocConsole();
         freopen_s(&fp, "CONIN$", "r", stdin);
         freopen_s(&fp, "CONOUT$", "w", stdout);
-        freopen_s(&fp, "CONOUT$", "w", stderr);
+        freopen_s(&fp, "CONOUT$", "w", stderr);*/
     }
 }
 
@@ -23,11 +31,13 @@ TriangleApp* TriangleApp::GetInstance() {
     return instance;
 }
 
-TriangleApp::TriangleApp(HINSTANCE hInstance, LPSTR lpCmdLine, int nCmdShow) : width(1280),
-                                                                            height(720),
-                                                                            shouldStop(false),
-                                                                            xMousePos(0),
-                                                                            yMousePos(0) {
+TriangleApp::TriangleApp(HINSTANCE hInstance, LPSTR lpCmdLine, int nCmdShow) :  width(1280),
+                                                                                height(720),
+                                                                                shouldStop(false),
+                                                                                xMousePos(0),
+                                                                                yMousePos(0),
+                                                                                d2dFactory(nullptr),
+                                                                                renderTarget(nullptr) {
     WNDCLASSEX wc;
     ZeroMemory(&wc, sizeof(WNDCLASSEX));
 
@@ -55,10 +65,73 @@ TriangleApp::TriangleApp(HINSTANCE hInstance, LPSTR lpCmdLine, int nCmdShow) : w
                                   NULL);    // used with multiple windows, NULL
 
     ShowWindow(windowHandle, nCmdShow);
+    InitializeD2D();
+}
+
+void TriangleApp::InitializeD2D() {
+    HRESULT hr = D2D1CreateFactory(
+        D2D1_FACTORY_TYPE_SINGLE_THREADED,
+        &d2dFactory
+    );
+
+    RECT rc;
+    GetClientRect(windowHandle, &rc);
+
+    hr = d2dFactory->CreateHwndRenderTarget(
+        D2D1::RenderTargetProperties(),
+        D2D1::HwndRenderTargetProperties(
+        windowHandle,
+        D2D1::SizeU(
+        rc.right - rc.left,
+        rc.bottom - rc.top)
+    ),
+        &renderTarget
+    );
+
+    if (SUCCEEDED(hr)) {
+        renderTarget->CreateSolidColorBrush(
+            D2D1::ColorF(D2D1::ColorF::White, 1.0f),
+            &brush
+        );
+    }
+
+    renderTarget->SetAntialiasMode(D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
+}
+
+void TriangleApp::OnDraw() {
+    HRESULT hr;
+    D2D1_SIZE_F renderTargetSize = renderTarget->GetSize();
+    D2D1_RECT_F brushRect = D2D1::RectF(100.0f, 100.0f, renderTargetSize.width - 100.0f, renderTargetSize.height - 100.0f);
+    D2D1_POINT_2F p0 = {100.0f, renderTargetSize.height - 100.0f};
+    D2D1_POINT_2F p1 = {renderTargetSize.width / 2.0f, 100.0f};
+    D2D1_POINT_2F p2 = {renderTargetSize.width - 100.0f, renderTargetSize.height - 100.0f};
+
+    renderTarget->BeginDraw();
+
+    renderTarget->Clear(D2D1::ColorF(37.0f / 256.0f, 133.0f / 256.0f, 75.0f / 256.0f, 1.0f));
+
+    renderTarget->DrawLine(p0, p1, brush);
+    renderTarget->DrawLine(p1, p2, brush);
+    renderTarget->DrawLine(p2, p0, brush);
+
+    hr = renderTarget->EndDraw();
+}
+
+void TriangleApp::OnResize(uint32_t width, uint32_t height) {
+    if (renderTarget) {
+        D2D1_SIZE_U size;
+        size.width = width;
+        size.height = height;
+
+        renderTarget->Resize(size);
+        OnDraw();
+    }
 }
 
 TriangleApp::~TriangleApp() {
-    delete instance;
+    SafeRelease(&renderTarget);
+    SafeRelease(&brush);
+    SafeRelease(&d2dFactory);
 }
 
 void TriangleApp::RunAppLoop() {
@@ -69,7 +142,7 @@ void TriangleApp::RunAppLoop() {
             TranslateMessage(&message);
             DispatchMessage(&message);
         }
-        std::cout << "Mouse X: " << xMousePos << " Y: " << yMousePos << std::endl;
+        OnDraw();
     }
 }
 
@@ -85,17 +158,29 @@ void TriangleApp::SetAppStop(bool shouldStop) {
 LRESULT TriangleApp::WndProc(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam) {
     TriangleApp* app = TriangleApp::GetInstance();
 
+    // Check whether application instance was created or not. If not pass current message processing to windows.
+    if (!app) return DefWindowProc(windowHandle, message, wParam, lParam);
+
     switch (message) {
     case WM_DESTROY:
         PostQuitMessage(0);
         app->SetAppStop(true);
+
         return 0;
     case WM_MOUSEMOVE:
         int xMousePos, yMousePos;
-        xMousePos = GET_X_LPARAM(lParam);
-        yMousePos = GET_Y_LPARAM(lParam);
+        xMousePos = LOWORD(lParam);
+        yMousePos = HIWORD(lParam);
 
         app->SetMousePos(xMousePos, yMousePos);
+
+        return 0;
+
+    case WM_SIZE:
+        uint32_t width = LOWORD(lParam);
+        uint32_t height = HIWORD(lParam);
+  
+        app->OnResize(width, height);
 
         return 0;
     }
--- TriangleApp/TriangleApp.h
+++ TriangleApp/TriangleApp.h
@@ -1,26 +1,38 @@
 #pragma once
-#include <windows.h>
+
 #include <cstdint>
 #include <memory>
+#include <windows.h>
+#include <d2d1.h>
 
 class TriangleApp {
 public:
     static void Create(HINSTANCE hInstance, LPSTR lpCmdLine, int nCmdShow);
     static TriangleApp* GetInstance();
 
-public:
     ~TriangleApp();
     void RunAppLoop();
     void SetMousePos(int x, int y);
     void SetAppStop(bool shouldStop);
 private:
-    TriangleApp(HINSTANCE hInstance, LPSTR lpCmdLine, int nCmdShow);
     static LRESULT CALLBACK WndProc(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam);
+
+    TriangleApp(HINSTANCE hInstance, LPSTR lpCmdLine, int nCmdShow);
+    void InitializeD2D();
+    void OnDraw();
+    void OnResize(uint32_t width, uint32_t height);
 private:
+    static TriangleApp* instance;
+    static bool isPresent;
+
     HWND windowHandle;
     uint32_t width, height;
     int xMousePos, yMousePos;
     bool shouldStop;
-    static TriangleApp* instance;
-    static bool isPresent;
+
+// Direct2D stuff
+private:
+    ID2D1Factory* d2dFactory;
+    ID2D1HwndRenderTarget* renderTarget;
+    ID2D1SolidColorBrush* brush;
 };
